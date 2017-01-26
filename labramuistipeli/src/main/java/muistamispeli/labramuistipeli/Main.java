564d717c4f64b3c90149381ea907787b47d47753
Mikko <mitami@ad.helsinki.fi>
2017-01-26 15:43:15 +0200

    luokat

--- labramuistipeli/src/main/java/muistamispeli/labramuistipeli/Main.java
+++ labramuistipeli/src/main/java/muistamispeli/labramuistipeli/Main.java
@@ -5,6 +5,9 @@
  */
 package muistamispeli.labramuistipeli;
 
+import java.util.Scanner;
+import muistamispeli.labramuistipeli.tekstikayttoliittyma.Tekstikayttoliittyma;
+
 /**
  *
  * @author mikko
@@ -13,5 +16,12 @@ public class Main {
     
     public static void main(String[] args) {
         System.out.println(" test");
+        
+        Scanner lukija = new Scanner(System.in);
+        Peli p = new Peli();
+        Tekstikayttoliittyma tk = new Tekstikayttoliittyma(p, lukija);
+        
+        tk.kaynnista();
+        
     }
 }
---
+++ labramuistipeli/src/main/java/muistamispeli/labramuistipeli/tekstikayttoliittyma/Tekstikayttoliittyma.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package muistamispeli.labramuistipeli.tekstikayttoliittyma;
+
+import java.util.Scanner;
+import muistamispeli.labramuistipeli.Peli;
+
+/**
+ *
+ * @author mikko
+ */
+public class Tekstikayttoliittyma {
+    private Peli p;
+    private Scanner lukija;
+    
+    public Tekstikayttoliittyma(Peli p, Scanner lukija) {
+        this.p = p;
+        this.lukija = lukija;
+    }
+    
+    public void kaynnista() {
+        System.out.println("Montako ruutua?");
+    }
+    
+    
+}
